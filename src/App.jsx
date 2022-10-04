a2159aacb04497480f405d33736b82add90262fd
DEREK-SNEIDER <esnei1595@gmail.com>
2022-10-03 23:22:11 -0500

    Primera pre-entrega del Proyecto Final

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+/* import "./App.css"; */
+
+import NavBar from "./component/NavBar/NavBar";
+
+
+
+function App() {
+
+
+  return (
+    <div className="master">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
